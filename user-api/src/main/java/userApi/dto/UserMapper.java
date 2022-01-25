fa91b924d6827408ed5224afb7a3c72f0ef677b8
sleabykhalil <khalilsleaby@hotmail.com>
2022-01-25 12:07:31 +0100

    correct bug mapstruct

--- user-api/src/main/java/userApi/dto/UserMapper.java
+++ user-api/src/main/java/userApi/dto/UserMapper.java
@@ -1,9 +1,7 @@
 package userApi.dto;
 
-import org.mapstruct.Mapper;
 import userApi.model.User;
 
-@Mapper(componentModel = "spring")
 public interface UserMapper {
 
     User userDtoToUser(UserDto userDto);
