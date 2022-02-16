19eabe3139d1aa1fc5893f6d80ee1a31a34c59b1
sleabykhalil <khalilsleaby@hotmail.com>
2022-02-16 21:00:44 +0100

    update openFiegn interfaces work ongoing

--- TourGuide/src/main/java/tourGuide/service/RewardsService.java
+++ TourGuide/src/main/java/tourGuide/service/RewardsService.java
@@ -33,7 +33,7 @@ public class RewardsService {
     GpsApi gpsApi;
     RewardApi rewardApi;
     UserApi userApi;
-    ExecutorService executorService = Executors.newFixedThreadPool(500);
+    ExecutorService executorService = Executors.newFixedThreadPool(300);
     // ExecutorService apiExecutorService = Executors.newFixedThreadPool(10);
     private final DateTimeHelper dateTimeHelper = new DateTimeHelper();
 
