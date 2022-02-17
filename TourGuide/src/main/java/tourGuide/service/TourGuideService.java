6a62254908942b6dc30573b90cac5e463e7bc73b
sleabykhalil <khalilsleaby@hotmail.com>
2022-02-17 12:31:09 +0100

    Set track user threads to 30 Set reward user threads to 1500

--- TourGuide/src/main/java/tourGuide/service/TourGuideService.java
+++ TourGuide/src/main/java/tourGuide/service/TourGuideService.java
@@ -33,8 +33,8 @@ public class TourGuideService {
     private final DateTimeHelper dateTimeHelper = new DateTimeHelper();
     GpsApi gpsApi;
     UserApi userApi;
-    ExecutorService executorService = Executors.newFixedThreadPool(50);
-    ExecutorService getRewardExecutorService = Executors.newFixedThreadPool(1000);
+    ExecutorService executorService = Executors.newFixedThreadPool(30);
+    ExecutorService getRewardExecutorService = Executors.newFixedThreadPool(1500);
 
     private Map<String, Boolean> trackUserMap = new ConcurrentHashMap<>();
 
