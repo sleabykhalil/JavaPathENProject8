d6a8e7643b51822662ab1ba0bd1f41fb38bf59d1
sleabykhalil <khalilsleaby@hotmail.com>
2022-02-27 23:07:09 +0100

    Add RestExceptionHandler

---
+++ TourGuide/src/main/java/tourGuide/exception/ValidationException.java
@@ -0,0 +1,19 @@
+package tourGuide.exception;
+
+
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+public class ValidationException extends RuntimeException {
+    public ValidationException(String message) {
+        super(message, null, false, false);
+    }
+
+    public ValidationException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public ValidationException(Throwable cause) {
+        super(cause);
+    }
+}
